6bd750d0cff8b82f69f039f71b250bf5c1c568d8
Polyxen1a <maslowa@inbox.ru>
2023-07-01 19:37:02 +0300

    Initial commit

---
+++ src/main/java/pro/sky/functionalinterfacesandlambda/FunctionalInterfacesAndLambdaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.functionalinterfacesandlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalInterfacesAndLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalInterfacesAndLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/functionalinterfacesandlambda/FunctionalInterfacesAndLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.functionalinterfacesandlambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalInterfacesAndLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
